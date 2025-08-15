8b31c10dad17d880f4aa32566253b4c9f0e3001d
LangerTwo <adrangel95@gmail.com>
2025-08-15 14:54:43 -0500

    first commit

---
+++ 01-reforzamiento/src/bases/01-cont-let.ts
@@ -0,0 +1,8 @@
+const firstName = 'Leo';
+const lastName = 'Nava';
+
+// console.log(firstName, lastName);
+
+const containsLetterN = lastName.includes('n');
+
+console.log({ containsLetterN })
---
+++ 01-reforzamiento/src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+import './bases/01-cont-let';
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Hi World!!</h1>
+  </div>
+`
