aef9bd724a28ec84558a373d979cd7aefa6e4bb2
LangerTwo <adrangel95@gmail.com>
2025-08-20 14:08:33 -0500

    interfaces y optimizacion

--- 01-reforzamiento/src/bases/10-fetch-api.ts
+++ 01-reforzamiento/src/bases/10-fetch-api.ts
@@ -1,19 +1,22 @@
+import type { Giphyrandomresponse, Gif } from "../data/giphy.response";
 
 
 const API_KEY = 'eJgXifTXOvWTWr3V5n8vDNBuBb3L5gCx';
 
 const myRequest = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`);
 
+const createImageInsideDOM = (url: string) => {
+    const imgElement = document.createElement('img');
+    imgElement.src = url;
+
+    document.body.append(imgElement);
+};
+
 myRequest
     .then((response) => response.json())
-    .then((data) => {
-        const imageUrl = data.data.images.original.url;
-        console.log(imageUrl);
-
-        const imgElement = document.createElement('img');
-        imgElement.src = imageUrl;
-
-        document.body.append(imgElement);
+    .then(({ data }: Giphyrandomresponse) => {
+        const imageUrl = data.images.original.url;
+        createImageInsideDOM(imageUrl);
     })
     .catch((err) => {
         console.error('Error:', err);
