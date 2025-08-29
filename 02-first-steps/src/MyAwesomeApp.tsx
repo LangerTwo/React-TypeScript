5380d2f343f586419457bede6ea2d8bb2e8b564d
LangerTwo <adrangel95@gmail.com>
2025-08-29 16:21:30 -0500

    impresion de variables

--- 02-first-steps/src/MyAwesomeApp.tsx
+++ 02-first-steps/src/MyAwesomeApp.tsx
@@ -1,8 +1,26 @@
+const firstName = 'Arthur';
+const lastName = 'Ainsworth';
+
+const favotiteGames = ['Metal Gear', 'Death Stranding', 'Elden Ring'];
+const isActive = true;
+
+const address = {
+    zipCode: 'ABC-123',
+    country: 'Canadá',
+};
 export function MyAwesomeApp() {
+
     return (
         <>
-            <h1>Arthur</h1>
-            <h3>Ainsworth</h3>
+            <h1>{ firstName }</h1>
+            <h3>{ lastName }</h3>
+
+            <p>{favotiteGames.join(', ')}</p>
+            <p>{ 2 + 2 }</p>
+
+            <h1>{isActive ? 'Activo' : 'No activo'}</h1>
+
+            <p>{JSON.stringify(address)}</p>
         </>
     );
 };
