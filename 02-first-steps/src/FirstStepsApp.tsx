0f04cefb55be7c2932b8e4ebaa34e794967b4871
LangerTwo <adrangel95@gmail.com>
2025-08-29 15:47:13 -0500

    creando componentes

---
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -0,0 +1,15 @@
+export function FirstStepsApp() {
+
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+
+            <button>Mi botón</button>
+
+            <div>
+                <h2>Listado de tareas</h2>
+            </div>
+        </>
+    );
+};
