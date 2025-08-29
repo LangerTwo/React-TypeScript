2f93708072033cb23c467aef3a1be694de351782
LangerTwo <adrangel95@gmail.com>
2025-08-29 16:55:12 -0500

    mostrar listados de elementos

--- 02-first-steps/src/FirstStepsApp.tsx
+++ 02-first-steps/src/FirstStepsApp.tsx
@@ -1,14 +1,33 @@
 import { ItemCounter } from "./shopping-cart/ItemCounter";
 
+interface ItemInCart {
+    productName: string;
+    quantity: number;
+};
+
+const itemsInCart: ItemInCart[] = [
+    { productName: 'Nintendo Switch 2', quantity: 15 },
+    { productName: 'Pro Controller', quantity: 20 },
+    { productName: 'Super Smash', quantity: 25 },
+    { productName: 'Super Mario', quantity: 30 },
+];
+
 export function FirstStepsApp() {
 
     return (
         <>
             <h1>Carriyo de Compras</h1>
 
-            <ItemCounter name="Nintendo Switch 2" quantity={15}/>
+            {
+                itemsInCart.map(({ productName, quantity }) => (
+                    <ItemCounter key={productName} name={productName} quantity={quantity} />
+                ))
+            }
+
+            {/* <ItemCounter name="Nintendo Switch 2" quantity={15}/>
             <ItemCounter name="Pro Controller" quantity={20}/>
             <ItemCounter name="Super Smash" quantity={25}/>
+            <ItemCounter name="Super Mario" quantity={30}/> */}
         </>
     );
 };
