845a675d730c6910e6a20ab5ae73898d4a21cdec
LangerTwo <adrangel95@gmail.com>
2025-09-02 16:32:02 -0500

    Hook - useState

--- 02-first-steps/src/shopping-cart/ItemCounter.tsx
+++ 02-first-steps/src/shopping-cart/ItemCounter.tsx
@@ -1,29 +1,43 @@
+import { useState } from "react";
+
 interface Props {
     name?: string,
     quantity?: number
 };
 
-export const ItemCounter = ({name, quantity}: Props) => {
-    console.log(name);
-  return (
-    <>
-        <section
-            style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '1rem',
-                marginTop: '2rem'
-            }}
-        >
-            <span
+export const ItemCounter = ({name, quantity = 1}: Props) => {
+    // console.log(name);
+    const [count, setCount] = useState(quantity);
+
+    const handleAdd = () => {
+        setCount(count + 1);
+    }
+
+    const handleSubTract = () => {
+        if (count === 0) return;
+
+        setCount(count - 1);
+    }
+
+    return (
+        <>
+            <section
                 style={{
-                    width: '200px',
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '1rem',
+                    marginTop: '2rem'
                 }}
-            >{name}</span>
-            <button>+1</button>
-            <span>{quantity}</span>
-            <button>-1</button>
-        </section>
-    </>
-  )
+            >
+                <span
+                    style={{
+                        width: '200px',
+                    }}
+                >{name}</span>
+                <button onClick={handleAdd}>+1</button>
+                <span>{count}</span>
+                <button onClick={handleSubTract}>-1</button>
+            </section>
+        </>
+    )
 }
