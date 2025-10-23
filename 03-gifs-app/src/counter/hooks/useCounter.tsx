79c494f5e7dde1b13e441bf8655d6f741faf2449
LangerTwo <adrangel95@gmail.com>
2025-10-23 15:16:58 -0500

    usando custom hooks

---
+++ 03-gifs-app/src/counter/hooks/useCounter.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useCounter = ( initialValue = 15 ) => {
+
+    const [counter, setCounter] = useState(initialValue);
+
+    const handleAdd = () => setCounter(counter + 1);
+
+    const handleSubtract = () => setCounter( prevState => prevState - 1 );
+
+    const handleReset = () => setCounter(initialValue);
+
+    return {
+        // Properties
+        counter,
+
+        // Methods
+        handleAdd,
+        handleSubtract,
+        handleReset,
+    };
+}
