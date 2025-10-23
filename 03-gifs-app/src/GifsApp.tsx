5fc823d6fae43d548dba8c76fb82618cb7b006ae
LangerTwo <adrangel95@gmail.com>
2025-10-23 13:31:11 -0500

    Obtener gifs mediante una peticion http

--- 03-gifs-app/src/GifsApp.tsx
+++ 03-gifs-app/src/GifsApp.tsx
@@ -4,17 +4,31 @@ import { PreviousSearches } from "./gifs/components/PreviousSearches"
 import { mockGifs } from "./mock-data/gifs.mock"
 import { CustomHeader } from "./shared/components/CustomHeader"
 import { SearchBar } from "./shared/components/SearchBar"
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query-action"
 
 export const GifsApp = () => {
     
     const [previousTerms, setPreviousTerms] = useState(['dark souls', 'bloodborne', 'elden ring', 'sekiro']);
 
     const handleTermClicked = (term: string) => {
-        console.log(`Term clicked: ${term}`);
+        console.log(`${term}`);
     };
 
-    const handleSerach = (query: string) => {
-        console.log(`Search term: ${query}`);
+    const handleSerach = async (query: string) => {
+        // console.log(`Search term: ${query}`);
+        query = query.trim().toLowerCase();
+        
+        if (query.length === 0) return;
+
+        if (previousTerms.includes(query)) return;
+
+        const currentTerms = previousTerms.slice(0, 6);
+        currentTerms.unshift(query);
+
+        setPreviousTerms( [query, ...previousTerms.splice(0, 8)] );
+
+        const gifs = await getGifsByQuery( query );
+        console.log(gifs);
     };
 
     return (
