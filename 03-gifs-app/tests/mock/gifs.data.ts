954b131e81e340f5a702a9114dc5d118da59accc
LangerTwo <adrangel95@gmail.com>
2025-10-28 16:05:24 -0500

    pruebas sobre accion - getGifsByQuery

---
+++ 03-gifs-app/tests/mock/gifs.data.ts
@@ -0,0 +1,72 @@
+export const gifsMock = [
+  {
+    id: 'WOb8EeFziTQNE02WXs',
+    title: 'Dragon Ball Super GIF by Toei Animation',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/WOb8EeFziTQNE02WXs/giphy.gif',
+    width: 540,
+    height: 304
+  },
+  {
+    id: 'B6SyssSlTgPXq',
+    title: 'Dragonball Goku Super Saiyan GIF',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/B6SyssSlTgPXq/giphy.gif',
+    width: 200,
+    height: 200
+  },
+  {
+    id: 'MQ08dsDSArNtEzf2kk',
+    title: '',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/MQ08dsDSArNtEzf2kk/giphy.gif',
+    width: 480,
+    height: 270
+  },
+  {
+    id: 'yNrDwdkOQj5CVe4B7w',
+    title: 'Dragon Ball Dbs GIF',
+    url: 'https://media2.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/yNrDwdkOQj5CVe4B7w/giphy.gif',
+    width: 480,
+    height: 480
+  },
+  {
+    id: 'u07Eq016TuYAFpmpfp',
+    title: 'Goku GIF',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/u07Eq016TuYAFpmpfp/giphy.gif',
+    width: 480,
+    height: 394
+  },
+  {
+    id: 'SIuI7syOPvm1HAd5GF',
+    title: 'Dragon Ball Super Ultra Instinct GIF by Toei Animation',
+    url: 'https://media1.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/SIuI7syOPvm1HAd5GF/giphy.gif',
+    width: 500,
+    height: 281
+  },
+  {
+    id: '28Uh9EJN06mvS',
+    title: 'son goku water GIF',
+    url: 'https://media2.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/28Uh9EJN06mvS/giphy.gif',
+    width: 500,
+    height: 500
+  },
+  {
+    id: 'dxld1UBIiGuoh31Fus',
+    title: 'Dragon Ball GIF by Toei Animation',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dxld1UBIiGuoh31Fus/giphy.gif',
+    width: 464,
+    height: 260
+  },
+  {
+    id: 'dmFXUZ5up1T896HP8B',
+    title: 'Dragon Ball Db GIF by BANDAI NAMCO',
+    url: 'https://media4.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/dmFXUZ5up1T896HP8B/giphy.gif',
+    width: 573,
+    height: 321
+  },
+  {
+    id: 'CY3A9zOlZR8uhFbeok',
+    title: 'Dragon Ball Super Ultra Instinct GIF by Toei Animation',
+    url: 'https://media0.giphy.com/media/v1.Y2lkPTI2MjZmZWYyeXBybXplM2MwYnFwenI1bGUwbTZ5bjh1dDZoNDNvMmVwOXM2cHZodyZlcD12MV9naWZzX3NlYXJjaCZjdD1n/CY3A9zOlZR8uhFbeok/giphy.gif',
+    width: 480,
+    height: 278
+  }
+]
