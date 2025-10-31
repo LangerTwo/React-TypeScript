fad54503a76c076dc318a3604884cd2fa3a9db59
LangerTwo <adrangel95@gmail.com>
2025-10-31 15:18:33 -0500

    tarea - custom hook

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,11 +1,12 @@
 // import { TrafficLight } from "./01-useState/TrafficLight"
-import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"
+// import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"
+import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook"
 
 const HooksApp = () => {
   return (
     <div className="bg-gradient">
         {/* <TrafficLight /> */}
-        <TrafficLightWithEffect />
+        <TrafficLightWithHook />
     </div>
   )
 }
---
+++ 04-hooks-app/src/hooks/useTrafficLight.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+
+const colors = {
+    red: "bg-red-500 animate-pulse",
+    yellow: "bg-yellow-500 animate-pulse",
+    green: "bg-green-500 animate-pulse",
+};
+
+type TrafficLightColor = keyof typeof colors;
+
+export const useTrafficLight = () => {
+  const [light, setLight] = useState<TrafficLightColor>("red");
+    const [countdown, setCountdown] = useState(5)
+
+    // Countdown effect
+    useEffect(() => {
+        if (countdown === 0) return;
+        
+        const intervalId = setInterval(() => {
+            setCountdown((prev) => prev - 1);
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    }, [countdown]);
+
+    // Light change effect
+    useEffect(() => {
+
+        if (countdown > 0) return;
+
+        setCountdown(5);
+        if (light === "red") {
+            setLight("green");
+            return;
+        }
+
+        if (light === "yellow") {
+            setLight("red");
+            return;
+        }
+
+        if (light === "green") {
+            setLight("yellow");
+            return;
+        };
+        
+    }, [light, countdown]);
+
+    return { light, countdown, colors };
+}
