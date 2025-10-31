be104e0506afd9a1f54a912c3856d53d547525a9
LangerTwo <adrangel95@gmail.com>
2025-10-31 16:00:47 -0500

    Conectar varios custom hooks pt 1

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,12 +1,12 @@
 // import { TrafficLight } from "./01-useState/TrafficLight"
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"
-import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook"
+// import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook"
+import { PokemonPage } from "./03-examples/PokemonPage"
 
 const HooksApp = () => {
   return (
     <div className="bg-gradient">
-        {/* <TrafficLight /> */}
-        <TrafficLightWithHook />
+        <PokemonPage />
     </div>
   )
 }
