b0694d4339e7d89425e0ae18f5a649b785a88328
LangerTwo <adrangel95@gmail.com>
2025-10-31 14:41:38 -0500

    useEffect - Disparar efectos secundarios

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,9 +1,11 @@
-import { TrafficLight } from "./01-useState/TrafficLight"
+// import { TrafficLight } from "./01-useState/TrafficLight"
+import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"
 
 const HooksApp = () => {
   return (
     <div className="bg-gradient">
-        <TrafficLight />
+        {/* <TrafficLight /> */}
+        <TrafficLightWithEffect />
     </div>
   )
 }
