2b64d8bec98d4203772500cd6699d45a93b1154f
LangerTwo <adrangel95@gmail.com>
2025-10-31 13:16:15 -0500

    preparndo el entorno

---
+++ 04-hooks-app/src/HooksApp.tsx
@@ -0,0 +1,11 @@
+import { TrafficLight } from "./01-useState/TrafficLight"
+
+const HooksApp = () => {
+  return (
+    <div className="bg-gradient">
+        <TrafficLight />
+    </div>
+  )
+}
+
+export default HooksApp
