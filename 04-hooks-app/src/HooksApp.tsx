1fa88cc47fc94f301aded18266dcd436fc1f881b
LangerTwo <adrangel95@gmail.com>
2025-10-31 16:49:00 -0500

    useRef - Valor que no dispara re-render

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,12 +1,13 @@
 // import { TrafficLight } from "./01-useState/TrafficLight"
 // import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightWithEffect"
 // import { TrafficLightWithHook } from "./02-useEffect/TrafficLightWithHook"
-import { PokemonPage } from "./03-examples/PokemonPage"
+// import { PokemonPage } from "./03-examples/PokemonPage"
+import { FocusScreen } from "./04-useRef/FocusScreen"
 
 const HooksApp = () => {
   return (
     <div className="bg-gradient">
-        <PokemonPage />
+        <FocusScreen />
     </div>
   )
 }
