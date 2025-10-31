fe95732af5302c92a147db1379293118e60004bb
LangerTwo <adrangel95@gmail.com>
2025-10-31 15:30:53 -0500

    extra tarea custom hook

--- 04-hooks-app/src/02-useEffect/TrafficLightWithHook.tsx
+++ 04-hooks-app/src/02-useEffect/TrafficLightWithHook.tsx
@@ -1,7 +1,7 @@
 import { useTrafficLight } from '../hooks/useTrafficLight';
 
 export const TrafficLightWithHook = () => {
-    const {light, countdown, colors} = useTrafficLight();
+    const {countdown, percentage, greenLight, yellowLight, redLight} = useTrafficLight();
 
     return (
         <div className="min-h-screen bg-linear-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
@@ -12,16 +12,16 @@ export const TrafficLightWithHook = () => {
 
             <div className="w-64 bg-gray-700 rounded-full h-2">
                 <div className="bg-blue-500 h-2 rounded-full transition-all duration-1000 ease-linear"
-                    style={{ width: `${((5 - countdown) / 5) * 100}%` }}
+                    style={{ width: `${percentage}%` }}
                 >
                 </div>
             </div>
 
-            <div className={`w-32 h-32 ${ light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+            <div className={`w-32 h-32 ${redLight} rounded-full`}></div>
 
-            <div className={`w-32 h-32 ${ light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+            <div className={`w-32 h-32 ${yellowLight} rounded-full`}></div>
 
-            <div className={`w-32 h-32 ${ light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
+            <div className={`w-32 h-32 ${greenLight} rounded-full`}></div>
         </div>
         </div>
     );
--- 04-hooks-app/src/hooks/useTrafficLight.tsx
+++ 04-hooks-app/src/hooks/useTrafficLight.tsx
@@ -46,5 +46,16 @@ export const useTrafficLight = () => {
         
     }, [light, countdown]);
 
-    return { light, countdown, colors };
+    return { 
+        // Pros
+        light, 
+        countdown, 
+        colors,
+
+        // Computed properties
+        percentage: (countdown) / 5 * 100,
+        greenLight: light === 'green' ? colors.green : 'bg-gray-500',
+        yellowLight: light === 'yellow' ? colors.yellow : 'bg-gray-500',
+        redLight: light === 'red' ? colors.red : 'bg-gray-500',
+    };
 }
