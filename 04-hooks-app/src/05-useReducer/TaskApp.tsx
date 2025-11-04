e046ca98b43c851d6a7d33e9210e74e186dd6e87
LangerTwo <adrangel95@gmail.com>
2025-11-04 12:47:32 -0500

    Controlar estados complejos - useReducer

--- 04-hooks-app/src/05-useReducer/TaskApp.tsx
+++ 04-hooks-app/src/05-useReducer/TaskApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 
 import { Plus, Trash2, Check } from 'lucide-react';
 
@@ -10,45 +10,25 @@ import { Input } from '../components/ui/input';
 import { Checkbox } from '../components/ui/checkbox';
 // import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Card, CardContent, CardHeader, CardTitle } from '../components/ui/card';
-
-interface Todo {
-  id: number;
-  text: string;
-  completed: boolean;
-}
+import { getTaskInitialState, taskReducer } from './reducer/taskReducer';
 
 export const TasksApp = () => {
-  const [todos, setTodos] = useState<Todo[]>([]);
   const [inputValue, setInputValue] = useState('');
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  const [state, dispatch] = useReducer( taskReducer, getTaskInitialState() )
 
   const addTodo = () => {
     if (inputValue.length === 0) return;
-
-    const newTodo: Todo = {
-      id: Date.now(),
-      text: inputValue.trim(),
-      completed: false,
-    };
-
-    setTodos([ ...todos, newTodo]);
-    // setTodos((prev) => [...prev, newTodo]);
+    dispatch({ type: 'ADD_TODO', payload: inputValue });
     setInputValue('');
   };
 
   const toggleTodo = (id: number) => {
-    const updatesTodos = todos.map(todo => {
-      if( todo.id === id) {
-        return{ ...todo, completed: !todo.completed}
-      }
-      return todo;
-    });
-
-    setTodos(updatesTodos);
+    dispatch({ type: 'TOGGLE_TODO', payload: id });
   };
 
   const deleteTodo = (id: number) => {
-    const uptadtedTodos = todos.filter((todo) => todo.id !==  id);
-    setTodos(uptadtedTodos);
+    dispatch({ type: 'DELETE_TODO', payload: id });
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
@@ -59,8 +39,10 @@ export const TasksApp = () => {
 
   };
 
-  const completedCount = todos.filter((todo) => todo.completed).length;
-  const totalCount = todos.length;
+  const { todos, completed: completedCount, length: totalCount } = state;
+
+  // const completedCount = todos.filter((todo) => todo.completed).length;
+  // const totalCount = todos.length;
 
   return (
     <div className=" w-full min-h-screen bg-linear-to-br from-slate-50 to-slate-100 p-4">
