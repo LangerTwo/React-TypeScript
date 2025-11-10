8cd468166da66605da668f27e037fe29e4959c21
LangerTwo <adrangel95@gmail.com>
2025-11-10 16:37:04 -0500

    Contexto de usuario - Información global

--- 04-hooks-app/src/09-useContext/context/UserContext.tsx
+++ 04-hooks-app/src/09-useContext/context/UserContext.tsx
@@ -1,11 +1,42 @@
-import React, { useState, type PropsWithChildren } from "react";
+import React, { createContext, useState, type PropsWithChildren } from "react";
+import type { User } from "../data/user-mock.data";
 
 // interface UserContextProps {
 //     children: React.ReactNode;
 // }
 
+type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
+
+interface UserContextProps {
+    //State
+    authStatus: AuthStatus;
+    user: User | null;
+
+    //Methods
+    login: (userId: number) => boolean;
+    logout: () => void;
+}
+
+export const UserContext = createContext({} as UserContextProps);
+
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
     // const [name, setName] = useState('Leo');
+    const [authStatus, setAuthStatus] = useState<AuthStatus>('checking');
+    const [user, setUser] = useState<User|null>(null);
 
-    return <>{children}</>
+    const handleLogin = (userId: number) => {
+        console.log({userId})
+        return true;
+    };
+
+    const handleLogout = () => {
+        console.log('logout')
+    };
+
+    return <UserContext value={{
+        authStatus: authStatus,
+        user: user,
+        login: handleLogin,
+        logout: handleLogout,
+    }}>{children}</UserContext>
 };
