1b52e3a7cc4c56d69cbcb631dd660f7a73ec65bb
LangerTwo <adrangel95@gmail.com>
2025-11-10 14:38:41 -0500

    ProfessionalApp

--- 04-hooks-app/src/HooksApp.tsx
+++ 04-hooks-app/src/HooksApp.tsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+// import { Suspense } from 'react'
 import { Toaster } from 'sonner'
 
 // import { TrafficLight } from "./01-useState/TrafficLight"
@@ -11,8 +11,9 @@ import { Toaster } from 'sonner'
 // import { MemoHook } from "./06-memos/MemoHook"
 // import { MemoCounter } from "./06-memos/MemoCounter"
 // import { InstagromApp } from "./07-useOptimistc/InstagromApp"
-import { ClientInformation } from './08-use-suspense/ClientInformation'
-import { getUserAction } from './08-use-suspense/get-user-action'
+// import { ClientInformation } from './08-use-suspense/ClientInformation'
+// import { getUserAction } from './08-use-suspense/get-user-action'
+import { ProfessionalApp } from './09-useContext/ProfessionalApp'
 
 // import './index.css'
 
@@ -20,13 +21,7 @@ const HooksApp = () => {
   return (
     <div className="bg-gradient">
       <Toaster />
-        <Suspense fallback={(
-          <div className='bg-linear flex flex-col'>
-            <h1 className='text-2xl'>Cargando</h1>
-          </div>
-        )}>
-          <ClientInformation getUser={getUserAction(100)}/>
-        </Suspense>
+      <ProfessionalApp />
     </div>
   )
 }
