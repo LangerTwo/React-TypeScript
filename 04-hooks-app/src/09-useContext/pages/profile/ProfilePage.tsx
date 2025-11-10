f39e3eea185ece939e0b4f02731ffb8ab33d2cbc
LangerTwo <adrangel95@gmail.com>
2025-11-10 17:07:02 -0500

    Consumir el contexto

--- 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
+++ 04-hooks-app/src/09-useContext/pages/profile/ProfilePage.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react"
 import { Button } from "../../../components/ui/button"
+import { UserContext } from "../../context/UserContext"
 
 export const ProfilePage = () => {
+  const { user } = useContext(UserContext)
+  
+
   return (
     <div className="flex flex-col items-center justify-center max-h-screen">
       <h1 className="text-4xl">Perfil del usuario</h1>
       <hr />
 
-      <pre className="my-4">{JSON.stringify({}, null, 2)}</pre>
+      <pre className="my-4 w-full overflow-auto">{JSON.stringify({user}, null, 2)}</pre>
       <Button variant="destructive">Salir</Button>
     </div>
   )
