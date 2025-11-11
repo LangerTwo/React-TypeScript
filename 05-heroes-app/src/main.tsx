619f2dc9ab6de8ca1957ebf9d84f6ef944254dbd
LangerTwo <adrangel95@gmail.com>
2025-11-11 17:49:46 -0500

    Creación de rutas y pantallas

--- 05-heroes-app/src/main.tsx
+++ 05-heroes-app/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Button } from './components/ui/button'
+import { HeroesApp } from './HeroesApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hello World!!</h1>
-    <Button>Boton</Button>
+    <HeroesApp />
   </StrictMode>,
 )
