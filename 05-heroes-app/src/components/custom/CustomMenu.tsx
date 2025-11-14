2a5f818030a3c5501022edb0c2ee274e05f8bc14
LangerTwo <adrangel95@gmail.com>
2025-11-14 15:39:01 -0500

    HeroPage

--- 05-heroes-app/src/components/custom/CustomMenu.tsx
+++ 05-heroes-app/src/components/custom/CustomMenu.tsx
@@ -10,7 +10,7 @@ export const CustomMenu = () => {
     }
 
     return (
-        <NavigationMenu>
+        <NavigationMenu className="py-5">
             <NavigationMenuList>
 
                 {/* Home */}
