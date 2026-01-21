809fb2617bbc95cfeb9a14eb64dd1daf7812b4b9
LangerTwo <adrangel95@gmail.com>
2026-01-21 17:12:29 -0500

    Add sonner for toast notifications and enhance login functionality with error handling

--- 07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
+++ 07-teslo-shop/teslo-shop/src/auth/pages/login/LoginPage.tsx
@@ -1,22 +1,37 @@
-import { Link } from "react-router"
+import { Link, useNavigate } from "react-router"
 
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { CustomLogo } from "@/components/custom/CustomLogo"
-import type { FormEvent } from "react"
+import { useState, type FormEvent } from "react"
+import { loginAction } from "@/auth/actions/login.action"
+import { toast } from "sonner"
 
 export function LoginPage() {
 
+  const navigate = useNavigate();
+  const [isPosting, setIsPosting] = useState(false);
+
   const handleLogin = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    setIsPosting(true);
 
     const formData = new FormData(event.target as HTMLFormElement);
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
 
-    console.log({ email, password });
+    try {
+      const data = await loginAction(email, password);
+      localStorage.setItem('token', data.token);
+      console.log('Login exitoso');
+      navigate('/');
+    } catch (error) {
+      toast.error('Error al iniciar sesión. Por favor, verifica tus credenciales.');
+    }
+
+    setIsPosting(false);
   };
 
   return (
@@ -42,7 +57,7 @@ export function LoginPage() {
                 </div>
                 <Input id="password" type="password" name="password" placeholder="Contraseña" required />
               </div>
-              <Button type="submit" className="w-full">
+              <Button type="submit" className="w-full" disabled={isPosting}>
                 Ingresar
               </Button>
               <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
